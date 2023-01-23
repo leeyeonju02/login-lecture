3f08894eb379229749467654637571bdade18512
leeyeonju02 <dlduswn0783@naver.com>
2023-01-23 17:24:41 +0900

    프론트 단 javascipt파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,10 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs"); //뷰엔진을 어떤 방식으로 해석할지
 
+//js에 접근할 수 있도록 미들웨어를 등록한다.
+app.use(express.static(`${__dirname}/src/public`)); //express의 메소드 중 정적 경로를 추가할 수 있는 메소드 -static
+//dirname은 현재app.js가 있는 위치를 반환한다. -> 그 위치(디렉토리네임) 안에, src 폴더 안에 public 폴더를 정적 경로로 추가(use)
+
 app.use("/", home); //use 미들웨어를 등록하는 메소드
 
 module.exports = app;
---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,5 @@
+"use strict";
+//html과 연동된 js 파일
+// login.ejs - login.js 연결
+//login.js는 html 문서와 연결된 자바스크립트 파일로서 프론트 단에서 동작하는 자바스크립트
+console.log("hi");
