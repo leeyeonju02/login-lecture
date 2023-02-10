4a2c0fc3e63d320f94a1df2445f832148ae17246
leeyeonju02 <dlduswn0783@naver.com>
2023-02-10 19:53:51 +0900

    UserStorage 모델(데이터) 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,23 +1,17 @@
 "use strict";
 
-//queryselector: 질의 선택자
-const id = document.querySelector("#id"), //파라미터에서 선택자에서 html의 값을 가져올 수 있다.
+const id = document.querySelector("#id"),
   psword = document.querySelector("#psword"),
   loginBtn = document.querySelector("button");
 
-//로그인 버튼이 클릭되면 login 함수 실행
 loginBtn.addEventListener("click", login);
 
 function login() {
-  //요청데이터니까 req변수에 담음- object로 선언
   const req = {
-    //object로 선언
-    id: id.value, //테그에 value라는 변수에 접근해 버튼이 클릭됬을 때 값을 가져온다.
+    id: id.value,
     psword: psword.value,
   };
-  // 위에서 가져온 데이터를 fetch를 통해 서버에 전달한다(네트워크 통신)
-  //fetch라는 것을 통해서 어떤 경로를 통해 데이터를 전달해줘야 한다.
-  // /login이라는 경로가 있다고 가정하고 전달하자 두번째 파라미터로 전달할 데이터를 넣어준다 (object 형태)
+
   fetch("/login", {
     method: "POST",
     headers: {
@@ -25,7 +19,6 @@ function login() {
     },
     body: JSON.stringify(req), //stringify: 단순히 req를 문자열로 바꿔주는 메소드
   })
-    //서버에서 응답한 데이터 받기- res 데이터가 파라미터로 전달 : promise라는 데이터가 날라오게 된다. 이를 받기 위해선 then을 한번더
     .then((res) => res.json()) // console.log(res.json()))
     .then((res) => {
       if (res.success) {
@@ -33,5 +26,8 @@ function login() {
       } else {
         alert(res.msg);
       }
+    })
+    .catch((err) => {
+      console.error(new Error("로그인 중 에러 발생"));
     });
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,8 +1,9 @@
-"use strict";
-//라우터가 클라이언트의 요청을 연결해주면 실체 요청에 해당하는 컨트롤러 부분
+"use strict"; //라우터가 클라이언트의 요청을 연결해주면 실체 요청에 해당하는 컨트롤러 부분
 
-//변수 만들어 그 안에 컨트롤러 함수넣어 연결하기
+const UserStorage = require("../../models/UserStorage"); //모듈 가져오기: 상위..상위..모델ㄴ..에서 접근
+
 const output = {
+  //단순히 렌더링 해주는 함수들 output 객체에 모아 빼주기
   hello: (req, res) => {
     res.render("home/index");
   },
@@ -11,30 +12,28 @@ const output = {
   },
 };
 
-const users = {
-  id: ["leeyj", "나개발", "김팀장"],
-  psword: ["1234", "2345", "3456"],
-}; //id와 psword를 리스트로 만들어줘서 같은 번지의 패스워드를 만들어줬다.
-
-//hello와 login 은 단순히 해당 페이지를 렌더링 해주는 함수인데 이 함수를 아웃풋으로 빼주기
 const process = {
   login: (req, res) => {
     const id = req.body.id,
       psword = req.body.psword;
+    //const UserStorage = new UserStorage(); //모듈로 가져온 클래스의 인스턴스 만들기 -> 데이터를 저장하고 있는 클래스는 굳이 인스턴스화 없이 다이렉트로 클래스자체에서 변수에 접근가능하게 하자
 
+    const users = UserStorage.getUsers("id", "psword");
+    //정적변수로 지정했기 때문에 클래스 자체에서 접근할 수 있다  -> 원래 console.log(UserStorage.users())
+    //받아오고 싶은 데이터를 getUsers 호출하는 부분에서 정해서 만들어준다.
+
+    const response = {}; //json 뒤의 파라미터로 넘어오는 것들을 object로 정리 - 응답하는 객체로 만들기
     if (users.id.includes(id)) {
       const idx = users.id.indexOf(id);
       if (users.psword[idx] === psword) {
-        return res.json({
-          success: true,
-        });
+        response.success = true; //response 응답을 객체로 만들어 success 키 값을 true로 주어준다.
+        return res.json(response); //success 키 값이 true인 response 객체를 던져준다.
       }
     }
 
-    return res.json({
-      success: false,
-      msg: "로그인에 실패하셨습니다. ",
-    });
+    response.success = false;
+    response.msg = "로그인에 실패하셨습니다.";
+    return res.json(response); // response 객체에 아이디 불일치 시 키값을 넣어 데이터를 던져준다.
   },
 };
 //해당 데이터와 프론트에서 전달한 데이터를 인증하는 과정을 만들어보자
